9988dd6f51811ab9052790038770dc554a998755
Ken Bannister <kb2ma@runbox.com>
2020-07-14 18:00:01 -0400

    Add proper HDLC and XON/XOFF handling when read serial

--- root_reader.go
+++ root_reader.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"github.com/kb2ma/daghead/internal/log"
 	"github.com/lunixbochs/struc"
 	"github.com/mikepb/go-serial"
+	"github.com/snksoft/crc"
 	"sync"
 )
 
-const HDLC_FLAG byte = 0x7E
+const (
+	HDLC_FLAG   byte = 0x7E
+	HDLC_ESCAPE byte = 0x7D
+	FLOW_ESCAPE byte = 0x12
+	FLOW_XON    byte = 0x11
+	FLOW_XOFF   byte = 0x13
+	FLOW_MASK   byte = 0x10
+)
+
+// These values really are constants, but a slice can't be a constant.
+var (
+	HDLC_FLAG_ARRAY     = []byte{HDLC_FLAG}
+	HDLC_FLAG_ESCAPED   = []byte{HDLC_ESCAPE, 0x5E}
+	HDLC_ESCAPE_ARRAY   = []byte{HDLC_ESCAPE}
+	HDLC_ESCAPE_ESCAPED = []byte{HDLC_ESCAPE, 0x5D}
+)
 
 const NOTIFICATION_ERROR int = 0
 
@@ -25,6 +42,17 @@ type Notification struct {
 	Arg2 uint16
 }
 
+func decodeHdlc(buf []byte) (replBuf []byte, err error) {
+	replBuf = bytes.ReplaceAll(buf, HDLC_FLAG_ESCAPED, HDLC_FLAG_ARRAY)
+	replBuf = bytes.ReplaceAll(replBuf, HDLC_ESCAPE_ESCAPED, HDLC_ESCAPE_ARRAY)
+	crcBuf := replBuf[len(replBuf)-2:len(replBuf)]
+
+	hash := crc.CalculateCRC(crc.X25, replBuf[:len(replBuf)-2])
+	if (byte(hash & 0xFF) != crcBuf[0]) || (byte((hash & 0xFF00) >> 8) != crcBuf[1]) {
+		err = errors.New("CRC decode not valid")
+	}
+	return
+}
 
 // Status type 0 is_sync; example [83 56 66 0 0 61 189]
 func readStatusFrame(statusType byte, data []byte) {
@@ -55,11 +83,35 @@ func readDataFrame() {
 	log.Println(log.INFO, "got data")
 }
 
+/*
+Reads incoming byte stream from serial port of root mote. Data formatted as HDLC frames.
+Data includes several types of status messages, log notifications, and UDP datagrams.
+
+HDLC frames incoming data with a 0x7E flag bytes to start and end a frame, as
+shown below.
+
+   ... 7E 7E xx xx xx xx xx xx 7E 7E ...
+
+Within a frame, an incoming 7E byte is escaped as the sequence 7D 5E. An
+incoming 7D byte is escaped as the sequence 7D 5D. Handling for this mechanism
+is in decodeHdlc(), after reception of the entire frame.
+
+Note: It seems possible to handle this HDLC escaping inline, but we have implemented
+the handling after reception of the entire frame to follow OpenVisualizer for consistency.
+
+Since the serial port uses software based XON/XOFF flow control, XON (0x11) and
+XOFF (0x13) bytes also must be escaped within a frame. The escaped value is sent
+XORed with a mask byte (0x10). So, an incoming XON is escaped as the sequence 0x12 0x01,
+and XOFF is escaped as the sequence 0x12 0x03. An incoming escape byte (0x12) is
+escaped as the sequence 0x12 0x02. Handling for these escaped sequences is inline
+in this function.
+*/
 func readSerial(wg *sync.WaitGroup, port *serial.Port) {
 	defer wg.Done()
 
 	frameBuf := make([]byte, 0, 10)
 	isInFrame := false
+	isEscapingFlow := false
 
 	for true {
 		buf := make([]byte, 1)
@@ -69,28 +121,49 @@ func readSerial(wg *sync.WaitGroup, port *serial.Port) {
 		}
 
 		// At startup, position in stream from device is indeterminate. So must
-		// synchronize on first sequence of 0x7E 0x7E flag bytes.
+		// synchronize on first sequence of 0x7E 0x7E framing bytes.
 		if buf[0] == HDLC_FLAG {
 			if isInFrame && (len(frameBuf) > 0) {
 				log.Printf(log.DEBUG, "ending frame, len %d\n", len(frameBuf))
-				log.Println(log.DEBUG, frameBuf)
-				switch frameBuf[0] {
-				case 'S':
-					readStatusFrame(frameBuf[3], frameBuf[4:4+len(frameBuf)-6])
-				case 'E':
-					readNotificationFrame(NOTIFICATION_ERROR, frameBuf[1:1+len(frameBuf)-3])
-				case 'D':
-					readDataFrame()
+				log.Printf(log.DEBUG, "[% X]\n", frameBuf)
+
+				decoded, err := decodeHdlc(frameBuf)
+				if err == nil {
+					switch frameBuf[0] {
+					case 'S':
+						readStatusFrame(decoded[3], decoded[4:len(decoded)])
+					case 'E':
+						readNotificationFrame(NOTIFICATION_ERROR, decoded[1:len(decoded)])
+					case 'D':
+						readDataFrame()
+					}
+				} else {
+					log.Println(log.ERROR, err)
 				}
 				isInFrame = false
+				// defensive; should not be escaping flow if receive HDLC_FLAG
+				isEscapingFlow = false
 			} else {
 				log.Println(log.DEBUG, "starting frame")
 				isInFrame = true
 				frameBuf = frameBuf[:0]
+				isEscapingFlow = false
 			}
 		} else {
 			if isInFrame {
-				frameBuf = append(frameBuf, buf[0])
+				if buf[0] == FLOW_ESCAPE {
+					isEscapingFlow = true
+				} else {
+					if isEscapingFlow {
+						// Expect escaped XON/XOFF/FLOW_ESCAPE byte
+						frameBuf = append(frameBuf, buf[0] ^ FLOW_MASK)
+						isEscapingFlow = false
+
+					// Disregard raw XON/XOFF bytes as data. They should have been escaped.
+					} else if (buf[0] != FLOW_XON) && (buf[0] != FLOW_XOFF) {
+						frameBuf = append(frameBuf, buf[0])
+					}
+				}
 			}
 		}
 	}
