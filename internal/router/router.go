2e73ab7a6065079cf642645c7426517693fd33f3
Ken Bannister <kb2ma@runbox.com>
2020-07-20 16:31:29 -0400

    Able to read RPL DAO

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,80 +1,213 @@
 package router
 
-const (
-	PAGE_ONE_DISPATCH byte  = 0xF1
-	CRITICAL_6LoRH byte     = 0x80
-	MASK_6LoRH byte         = 0xE0
-	TYPE_6LoRH_RPI byte     = 0x05
-	IANA_IPv6HOPHEADER byte = 0
-    RPI_FLAG_MASK byte      = 0x1F
-    RPI_I_FLAG byte         = 0x02
-    RPI_K_FLAG byte         = 0x01
-    // there is no IANA for IPV6 HEADER right now, we use NHC identifier for it
-    // https://tools.ietf.org/html/rfc6282#section-4.2
-    IPV6_HEADER byte        = 0xEE
+import (
+	"errors"
+	"fmt"
+	"github.com/kb2ma/daghead/internal/log"
 )
 
+const (
+	PAGE_ONE_DISPATCH  byte = 0xF1
+	CRITICAL_6LoRH     byte = 0x80
+	MASK_6LoRH         byte = 0xE0
+	TYPE_6LoRH_RPI     byte = 0x05
+	IANA_IPv6HOPHEADER byte = 0
+	IANA_ICMPv6        byte = 0x3A
+	RPI_FLAG_MASK      byte = 0x1F
+	RPI_O_FLAG         byte = 0x10
+	RPI_R_FLAG         byte = 0x08
+	RPI_I_FLAG         byte = 0x02
+	RPI_K_FLAG         byte = 0x01
+	// there is no IANA for IPV6 HEADER right now, we use NHC identifier for it
+	// https://tools.ietf.org/html/rfc6282#section-4.2
+	IPV6_HEADER        byte = 0xEE
+	IPHC_TF_ELIDED     byte = 3
+	IPHC_NH_INLINE     byte = 0
+	IPHC_HLIM_64       byte = 2
+	IPHC_CID_NONE      byte = 0
+	IPHC_SAC_STATEFUL  byte = 1
+	IPHC_SAM_64B       byte = 1
+	IPHC_DAC_STATEFUL  byte = 1
+	IPHC_DAM_64B       byte = 1
+
+	RPL_TYPE_TRANSIT_INFORMATION byte = 0x06
+	RPL_TYPE_TARGET_INFORMATION byte  = 0x05
+)
+
+var (
+	NETWORK_PREFIX  = [8]byte{0xBB, 0xBB, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+)
+
+// Container for parsed IP header contents
+type IpData struct {
+	Source [16]byte
+	Dest [16]byte
+	Fields map[string]int
+}
+
 /*
-Read a data packet from the root node, and returns a map of 6LoWPAN field data
-found.
+Reads a data packet from the root node, and returns a map of 6LoWPAN field data
+found. Initializes provided IpData as needed.
 
 Presently, supports only reading RPL DAO to maintain source routing southbound
 into the mesh.
 */
-func ReadData(preHop byte, data []byte) (map[string]int) {
-	ipFields := make(map[string]int)
-	ipFields["pre_hop"] = int(preHop)
+func ReadData(ip *IpData, preHop byte, data []byte) (err error) {
+	if ip.Fields == nil {
+		ip.Fields = make(map[string]int)
+	}
+	ip.Fields["pre_hop"] = int(preHop)
 
 	// RFC 8025
-	// Expect 6LoWPAN adaptation header to begin with a context switch to Page 1.
+	// Expect 6LoWPAN adaptation header to begin with a parsing context switch
+	// to Page 1.
 	i := 0
 	if data[i] == PAGE_ONE_DISPATCH {
 		// RFC 8138
-		// Handle when the next two bytes specify RPL Packet Information type of
-		// 6LoWPAN routing header. The first byte specifies a 6LoRH hop-by-hop header
-		// (0b100xxxxx), as well as flags for RPI. The 6LoRH type of RPI is specified
-		// in the second byte.
+		// Read 6LoRH-RPI (critical) 0b100xxxxx header.
+		//  0                   1                   2
+		//  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+		// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+  ...  -+-+-+
+		// |1|0|0|O|R|F|I|K| 6LoRH Type=5  |   Compressed fields  |
+		// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+  ...  -+-+-+
 		i++
 		if (data[i] & MASK_6LoRH == CRITICAL_6LoRH) && (data[i+1] == TYPE_6LoRH_RPI) {
-			ipFields["next_header"] = int(IANA_IPv6HOPHEADER)
+			ip.Fields["next_header"] = int(IANA_IPv6HOPHEADER)
 			// RPI flags in the 5 least signficiant bits of the first byte.
-			ipFields["hop_flags"] = int(data[i] & RPI_FLAG_MASK)
+			ip.Fields["hop_flags"] = int(data[i] & RPI_FLAG_MASK)
 			i += 2
 
 			// Next 0 or 1 byte is RPL Instance ID
-			if ipFields["hop_flags"] & int(RPI_I_FLAG) == 0 {
-				ipFields["hop_rplInstanceID"] = int(data[i])
+			if ip.Fields["hop_flags"] & int(RPI_I_FLAG) == 0 {
+				ip.Fields["hop_rplInstanceID"] = int(data[i])
 				i++
 			} else {
 				// elided when only one RPL instance
-				ipFields["hop_rplInstanceID"] = 0
+				ip.Fields["hop_rplInstanceID"] = 0
 			}
 
 			// Next 1 or 2 bytes RPL sender rank. If one byte, must be a multiple
 			// of 256, so LSB elided.
-			if ipFields["hop_flags"] & int(RPI_K_FLAG) == 0 {
-				ipFields["hop_senderRank"] = (int(data[i]) << 8) + int(data[i+1])
+			if ip.Fields["hop_flags"] & int(RPI_K_FLAG) == 0 {
+				ip.Fields["hop_senderRank"] = (int(data[i]) << 8) + int(data[i+1])
 				i += 2
 			} else {
-				ipFields["hop_senderRank"] = int(data[i]) << 8
+				ip.Fields["hop_senderRank"] = int(data[i]) << 8
 				i++
 			}
 
 			// expect IPHC after 6LoRH RPI
-			ipFields["hop_next_header"] = int(IPV6_HEADER)
+			ip.Fields["hop_next_header"] = int(IPV6_HEADER)
 
 		} else {
 			// No support yet for handling 6LoRH of IP in IP or deadline,
 			// as implemented in OpenVisualizer.
 		}
+
+	// RFC 6282
+	// Read IPHC compressed IP header, 2 bytes
+	//   0                                       1
+	//   0   1   2   3   4   5   6   7   8   9   0   1   2   3   4   5
+	// +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+	// | 0 | 1 | 1 |  TF   |NH | HLIM  |CID|SAC|  SAM  | M |DAC|  DAM  |
+	// +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+	} else {
+		if (data[0] >> 5) != 0x03 {
+			err = errors.New(fmt.Sprintf("not a 6LowPAN IPHC header 0x%X", data[0]))
+			return
+		}
+
+		// next byte after IPHC header
+		i = 2
+		// Traffic Class and Flow Label
+		tf := (data[0] >> 3) & 0x3
+		if tf == IPHC_TF_ELIDED {
+			ip.Fields["flow_label"] = 0
+		} else {
+			log.Println(log.WARN, "unsupported IPHC TF value")
+		}
+
+		// Next Header
+		nh := (data[0] >> 2) & 0x1
+		if nh == IPHC_NH_INLINE {
+			ip.Fields["next_header"] = int(data[i])
+			i++
+		} else {
+			log.Println(log.WARN, "unsupported IPHC NH value")
+		}
+
+		// Hop limit
+		hlim := data[0] & 0x3
+		if hlim == IPHC_HLIM_64 {
+			ip.Fields["hop_limit"] = 64
+		} else {
+			log.Println(log.WARN, "unsupported IPHC HLIM value")
+		}
+
+		// Context Identifier extension
+		cid := (data[1] >> 7) & 0x1
+		if cid != IPHC_CID_NONE {
+			log.Println(log.WARN, "unsupported IPHC CID value")
+		}
+
+		// Source Address Compression
+		sac := (data[1] >> 6) & 0x1
+		if sac == IPHC_SAC_STATEFUL {
+			copy(ip.Source[:8], NETWORK_PREFIX[:])
+		} else {
+			log.Println(log.WARN, "unsupported IPHC SAC value")
+		}
+
+		// Source Address Mode
+		sam := (data[1] >> 4) & 0x3
+		if sam == IPHC_SAM_64B {
+			copy(ip.Source[8:16], data[i:i+8])
+			i += 8
+		} else {
+			log.Println(log.WARN, "unsupported IPHC SAM value")
+		}
+
+		// Multicast Compression (ignored)
+
+		// Destination Address Compression
+		dac := (data[1] >> 2) & 0x1
+		if dac == IPHC_DAC_STATEFUL {
+			copy(ip.Dest[:8], NETWORK_PREFIX[:])
+		} else {
+			log.Println(log.WARN, "unsupported IPHC DAC value")
+		}
+
+		// Destination Address Mode
+		dam := data[1] & 0x3
+		if dam == IPHC_DAM_64B {
+			copy(ip.Dest[8:16], data[i:i+8])
+			i += 8
+		} else {
+			log.Println(log.WARN, "unsupported IPHC DAM value")
+		}
+
+		// Next header
+		// Skip IPHC_NH_COMPRESSED handling; unsupported presently
+		// Skip next header is hop-by-hop handling; unsupported presently
 	}
 
 	// payload
-	ipFields["version"] = 6
-	ipFields["traffic_class"] = 0
-	ipFields["payload"] = i
-	ipFields["payload_length"] = len(data) - i
+	ip.Fields["version"] = 6
+	ip.Fields["traffic_class"] = 0
+	ip.Fields["payload"] = i
+	ip.Fields["payload_length"] = len(data) - i
 
-	return ipFields
+	return
+}
+
+func ReadRpl(source *[16]byte, data []byte) {
+	// skip DAO header
+	i := 20
+	log.Printf(log.INFO, "DAO from [% X]", source[8:])
+	if data[i] == RPL_TYPE_TRANSIT_INFORMATION {
+		// skip transit info header
+		i += 6
+		log.Printf(log.INFO, "parent [% X]", data[i+8:i+16])
+	}
 }
 
--- root_reader.go
+++ root_reader.go
@@ -50,12 +50,13 @@ func decodeHdlc(buf []byte) (replBuf []byte, err error) {
 	replBuf = bytes.ReplaceAll(buf, HDLC_FLAG_ESCAPED, HDLC_FLAG_ARRAY)
 	replBuf = bytes.ReplaceAll(replBuf, HDLC_ESCAPE_ESCAPED, HDLC_ESCAPE_ARRAY)
 	crcBuf := replBuf[len(replBuf)-2:]
+	replBuf = replBuf[:len(replBuf)-2]
 
-	hash := crc.CalculateCRC(crc.X25, replBuf[:len(replBuf)-2])
+	hash := crc.CalculateCRC(crc.X25, replBuf)
 	if (byte(hash & 0xFF) != crcBuf[0]) || (byte((hash & 0xFF00) >> 8) != crcBuf[1]) {
 		err = errors.New("CRC decode not valid")
 	}
-	return
+	return 
 }
 
 // Handles only status type 0, is_sync; example [83 56 66 0 0 61 189]
@@ -83,28 +84,69 @@ func readNotificationFrame(notificationLevel int, data []byte) {
 	}
 }
 
-/*
-example
-00  44 38 42 C3 2D 00 00 00 46 1D 52 44 7B 43 76 78 82 54 7D 13
-20  76 65 79 78 F1 83 05 0B 7A 55 3A 82 54 7D 13 76 65 79 78 46
-40  1D 52 44 7B 43 76 78 9B 02 E1 08 00 40 00 01 BB BB 00 00 00
-60  00 00 00 46 1D 52 44 7B 43 76 78 06 14 00 00 00 AA BB BB 00
-80  00 00 00 00 00 46 1D 52 44 7B 43 76 78 B4 D1
-*/
 func readDataFrame(data []byte) {
 	// skip mote ID [:2], asn [2:7], destination [7:15], source [15:23]
+	log.Printf(log.DEBUG, "Decoded: [% X]\n", data)
 	log.Printf(log.INFO, "got data; len total %d, payload %d\n", len(data), len(data)-23)
 	if len(data) < 23 {
+		log.Printf(log.ERROR, "Data frame payload length too small %d/n", len(data))
 		return
 	}
-	//hasHopByHopHeader := false
-	ipFields := router.ReadData(data[23], data[24:])
+	hasHopByHopHeader := false
+	ipData := new(router.IpData)
+	i := 23
+	if err := router.ReadData(ipData, data[22], data[i:]); err != nil {
+		log.Println(log.ERROR, err)
+		return
+	}
+	i += ipData.Fields["payload"]
+	log.Printf(log.DEBUG, "data frame pos/len %d/%d\n", i, ipData.Fields["payload_length"])
 
-	if ipFields["next_header"] == int(router.IANA_IPv6HOPHEADER) {
-		//hasHopByHopHeader := true
-		ipFields["next_header"] = ipFields["hop_next_header"]
-		// read inner header, expected to be IPHC (RFC 6282)
-		//innerFields := router.ReadData(data[23], data[ipFields["payload"]:])
+	if ipData.Fields["next_header"] == int(router.IANA_IPv6HOPHEADER) {
+		hasHopByHopHeader = true
+		ipData.Fields["next_header"] = ipData.Fields["hop_next_header"]
+	}
+
+	if ipData.Fields["next_header"] == int(router.IPV6_HEADER) {
+		hopLimit := ipData.Fields["hop_limit"]
+		// Read inner header, expected to be IPHC.
+		// Overwrites values from initial ReadData(), but that's OK except for
+		// hop limit. Note OpenVisualizer works differently. It copies individual
+		// fields after this second ReadData(). It's possible that the approach
+		// here, although simpler, will be problematic in other scenarios.
+		router.ReadData(ipData, data[22], data[i:])
+		if hopLimit != ipData.Fields["hop_limit"] {
+			ipData.Fields["hop_limit"] = hopLimit
+		}
+		i += ipData.Fields["payload"]
+		log.Printf(log.DEBUG, "data frame pos/len %d/%d\n", i, ipData.Fields["payload_length"])
+
+		if hasHopByHopHeader {
+			if (ipData.Fields["hop_flags"] & int(router.RPI_O_FLAG)) == int(router.RPI_O_FLAG) {
+				log.Printf(log.ERROR, "Packet was expected to move down into mesh from [% X]",
+				           ipData.Source)
+			}
+			if (ipData.Fields["hop_flags"] & int(router.RPI_R_FLAG)) == int(router.RPI_R_FLAG) {
+				log.Printf(log.ERROR, "Possible routing loop in packet from [% X]",
+				           ipData.Source)
+			}
+		}
+	}
+
+	if ipData.Fields["next_header"] == int(router.IANA_ICMPv6) {
+		if ipData.Fields["payload_length"] < 5 {
+			log.Printf(log.ERROR, "ICMP payload length too small %d/n",
+			           ipData.Fields["payload_length"])
+			return
+		}
+
+		ipData.Fields["icmpv6_type"] = int(data[i])
+		ipData.Fields["icmpv6_code"] = int(data[i+1])
+		ipData.Fields["icmpv6_checksum"] = int(data[i+2])
+		i += 4
+		ipData.Fields["app_payload"] = i
+
+		router.ReadRpl(&ipData.Source, data[i:])
 	}
 }
 
